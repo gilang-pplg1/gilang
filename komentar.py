01c4b481e66c82b3d2dfb933ac9e1a70de91e1c5
gilang-pplg1 <gilangramburabbani575@gmail.com>
2025-05-15 13:07:50 +0700

    tugas 1

---
+++ komentar.py
@@ -0,0 +1,14 @@
+
+"""
+Penulisan Komentar lebih dari satu baris yaitu
+dengan menggunakan kutip dua 3 kali dan
+ditutup dengan kutip dua 3 kali juga
+"""
+print("Hello semuanya")
+"""
+print("Hello semuanya") # ini komentar
+"""
+
+print("nama saya gilang")
+
+print("umur saya 16 tahun")
