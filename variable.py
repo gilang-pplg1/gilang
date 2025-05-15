1608d172c608f4a3743445090749c82b511d4288
gilang-pplg1 <gilangramburabbani575@gmail.com>
2025-05-15 13:09:23 +0700

    tugas 3

---
+++ variable.py
@@ -0,0 +1,29 @@
+# Proses memasukkan data ke dalam variabel
+nama = "Gilang"
+# Proses mencetak variabel
+print(nama)
+# Nilai dan tipe data dalam variabel dapat diubah
+umur = 16               # Nilai awal
+print(umur)             # Mencetak nilai umur
+print(type(umur))      # Mengecek tipe data umur
+umur = "enam belas" # Nilai setelah diubah
+print(umur)             # Mencetak nilai umur
+print(type(umur))      # Mengecek tipe data umur
+# Menggabungkan nama depan dan nama belakang
+namaDepan = "Rambu"
+namaBelakang = "Rabbani"
+nama = namaDepan + " " + namaBelakang
+umur = 17
+hobi = "olahraga"
+print("Biodata:\n", nama, "\nUmur:", umur, "\nHobi:", hobi)
+# Contoh variabel lainnya
+kalimat_sapaan = "Selamat Pagi"
+kalimatsapaan = "Selamat Siang"
+_kalimatsapaan = "Selamat Malam"
+kalimatsapaan222 = "Sampai Jumpa"
+print(kalimat_sapaan,kalimatsapaan,_kalimatsapaan,kalimatsapaan222, sep="\n")
+# Menghitung luas persegi panjang
+panjang = 15
+lebar = 8
+luas = panjang * lebar
+print("Luas Persegi Panjang:", luas)
