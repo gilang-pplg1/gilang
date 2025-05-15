cc5c998ae5a88ea1edb0669373ef5f692e711488
gilang-pplg1 <gilangramburabbani575@gmail.com>
2025-05-15 14:45:14 +0700

    tugas 4

---
+++ operator.py
@@ -0,0 +1,39 @@
+# perkslisn 
+panjang = 20
+lebar = 15
+luas = panjang * lebar
+print(luas)
+
+# pengurangan
+hutang = 18
+bayar = 10
+sisahutang = hutang - bayar
+print("sisa hutang adalah" ,sisahutang)
+
+# petambahan
+print(17 + 8)
+pisang = 10
+jeruk = 3
+buah = pisang + jeruk
+print(buah)
+
+# pembagian
+buku = 18
+siswa = 36
+bukupersiswa = buku / siswa
+print (bukupersiswa)
+
+# pangkat
+bilangan1 = 5
+bilangan2 = 4
+hasilpangkat = bilangan1 ** bilangan2
+print(hasilpangkat)
+
+# sisa bagi / modulus
+bilangan1 = 14
+bilangan2 = 7
+hasil = bilangan1 % bilangan2
+print("Sisa bagi dari bilangan ", bilangan1, " dan ", bilangan2, " adalah ", hasil)
+
+#pembagian bulat
+print(10/9)
