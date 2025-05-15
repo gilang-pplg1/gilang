1b0c28da7ce2e1def31475cd348463f0a1a998de
gilang-pplg1 <gilangramburabbani575@gmail.com>
2025-05-15 13:08:43 +0700

    tugas 2

---
+++ tipe data.py
@@ -0,0 +1,49 @@
+print("=== Tipe Data di Python ===\n")
+
+print("Boolean:")
+print("True  ->", True)
+print("False ->", False)
+
+
+print("String:")
+print("Halo Dunia!")
+print('Python itu asyik!')
+
+
+print("Integer:")
+print(123)
+print(-45)
+
+
+print("Float:")
+print(3.14)
+print(-22.7)
+
+
+print("Hexadecimal:")
+print("Bentuk hexadecimal dari 255 adalah", hex(255))
+
+
+print("Complex:")
+print(4 + 5j)
+
+
+print("List:")
+
+print([10, 'dua puluh', 30.5, True])
+
+
+print("Tuple:")
+print(('a', 100, 3.14))
+
+
+print("Dictionary:")
+print({'nama': 'Siti', 'umur': 30})
+
+
+print("=== Contoh penggunaan tipe data dan cek tipe datanya ===")
+x = '30'
+print("x =", x)
+print("Tipe data x adalah:", type(x))
+
+
